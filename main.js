4c7c1a82c8a5b1bde0943899029d4569188bb59a
Victor Wingert de Almeida <victor.wingert@estudante.ufjf.br>
2023-11-28 22:50:22 -0300

    arrumei o index e o main e criei o estoque

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
+const olJoao = document.querySelector("#joao");
+const olMaria = document.querySelector("#maria");
+
 
 document.entrada.addEventListener('submit', leFormulario);
 
 function leFormulario(event){
     event.preventDefault();
-    const quantidade = document.entrada.quantidade.value;
+    const quantidade = Number(document.entrada.quantidade.value);
     const fruta = document.entrada.fruta.value;
     const origem = document.entrada.origem.value;
+    const destino = document.entrada.destino.value;
 
-    console.log(`${origem} doa ${quantidade} ${fruta}`);
+    console.log(`${origem} doa ${quantidade} ${fruta} para ${destino}`);
 }
